834d561785b05124b6ec276100b74645738896a2
seyoung <seyoung8244@gmail.com>
2023-05-06 11:20:19 +0900

    Create

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Management System
+
+본 프로젝트는 리액트 강의목적으로 만들어진 고객관리시스텝입니다.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='gray-background'>
+        <h2>Let's develop management System</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
