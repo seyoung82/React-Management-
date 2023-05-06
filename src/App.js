a55172de497619d8a1dcb55db6a98d525d27a346
seyoung <seyoung8244@gmail.com>
2023-05-06 12:03:59 +0900

    Create Customer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@
 
 import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "92000",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "홍길동2",
+    birthday: "222222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "홍길동3",
+    birthday: "922122",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className='gray-background'>
-        <h2>Let's develop management System</h2>
-      </div>
+      <>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.gender}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </>
     );
   }
 }
